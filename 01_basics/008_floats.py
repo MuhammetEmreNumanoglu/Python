20c2a6c42b7f2499b3bb56c42527a3c0f9a276bd
Muhammet Emre Numanoglu <muhammedemrenumanoglu155@gmail.com>
2026-06-28 20:51:25 +0300

    Numbers - Integers - Floats - Booleans - Strings

---
+++ 01_basics/008_floats.py
@@ -0,0 +1,17 @@
+price = 9.99
+pi = 3.14159
+temperature = -4.5
+
+print(type(price))
+print(price * 3)
+print(round(pi, 2))
+print(round(2.675, 2))
+
+import math
+print(math.floor(3.7))
+print(math.ceil(3.2))
+print(math.isnan(float("nan")))
+print(math.isinf(float("inf")))
+
+print(0.1 + 0.2)
+print(round(0.1 + 0.2, 2))

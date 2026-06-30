28a4f389dd844d2616519d3571a3fb41a73c4f05
Muhammet Emre Numanoglu <muhammedemrenumanoglu155@gmail.com>
2026-06-30 14:35:05 +0300

    Operator Precedence - Math Functions - F String

---
+++ 01_basics/022_operator_precedence.py
@@ -0,0 +1,14 @@
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+print(2 ** 3 ** 2)
+print((2 ** 3) ** 2)
+
+print(10 / 2 + 3 * 4 - 1)
+
+print(True + 2 * 3)
+print(not False and True or False)
+print(not (False and True or False))
+
+result = 1 + 2 * 3 ** 2 / 3 - 1
+print(result)

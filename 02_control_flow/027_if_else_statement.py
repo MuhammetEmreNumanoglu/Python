13678c4be7cf231a8978d2d7b3208744986c982d
Muhammet Emre Numanoglu <muhammedemrenumanoglu155@gmail.com>
2026-06-30 14:37:04 +0300

    If Statements

---
+++ 02_control_flow/027_if_else_statement.py
@@ -0,0 +1,12 @@
+age = 17
+
+if age >= 18:
+    print("Access granted")
+else:
+    print("Access denied")
+
+number = -5
+if number >= 0:
+    print("Positive")
+else:
+    print("Negative")
---
+++ 02_control_flow/029_nested_if.py
@@ -0,0 +1,17 @@
+is_member = True
+age = 20
+
+if is_member:
+    if age >= 18:
+        print("Full member access")
+    else:
+        print("Junior member access")
+else:
+    print("No access - please register")
+
+x = 15
+if x > 10:
+    if x > 20:
+        print("Greater than 20")
+    else:
+        print("Between 10 and 20")

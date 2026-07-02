d9b4b07f190af4140d53c9b42c9925025c39a3ee
Muhammet Emre Numanoglu <muhammedemrenumanoglu155@gmail.com>
2026-07-02 16:47:36 +0300

    Loops

---
+++ 02_control_flow/032_for_loop.py
@@ -0,0 +1,17 @@
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    print(fruit)
+
+for letter in "Python":
+    print(letter, end=" ")
+print()
+
+for i in range(5):
+    print(i, end=" ")
+print()
+
+total = 0
+for num in [1, 2, 3, 4, 5]:
+    total += num
+print("Total:", total)

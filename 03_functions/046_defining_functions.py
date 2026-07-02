d34fe2800b7ba2d1fe857262484a78c9cf44cbbe
Muhammet Emre Numanoglu <muhammedemrenumanoglu155@gmail.com>
2026-07-03 00:27:29 +0300

    Function Defining - Parameters

---
+++ 03_functions/046_defining_functions.py
@@ -0,0 +1,16 @@
+def greet():
+    print("Hello!")
+
+def say_goodbye():
+    print("Goodbye!")
+
+greet()
+say_goodbye()
+greet()
+
+def print_separator():
+    print("-" * 20)
+
+print_separator()
+print("Content")
+print_separator()

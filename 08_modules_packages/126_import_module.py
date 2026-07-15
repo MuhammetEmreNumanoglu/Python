778b8c5a80b743400f335a48bdcb4e1bd4e5ee80
Muhammet Emre Numanoğlu <muhammedemrenumanoglu155@gmail.com>
2026-07-15 23:14:08 +0300

    Import Module - From Import

---
+++ 08_modules_packages/126_import_module.py
@@ -0,0 +1,9 @@
+import math
+import os
+import sys
+
+print(math.pi)
+print(math.sqrt(16))
+print(os.getcwd())
+print(sys.version)
+print(sys.platform)
---
+++ 08_modules_packages/127_from_import.py
@@ -0,0 +1,11 @@
+from math import pi, sqrt, floor, ceil
+from os import getcwd, listdir
+from sys import version, platform
+
+print(pi)
+print(sqrt(25))
+print(floor(3.7))
+print(ceil(3.2))
+print(getcwd())
+print(platform)
+print(version[:5])

f4f027a088e562f9094aa368141e4854bb09f2a6
Muhammet Emre Numanoğlu <muhammedemrenumanoglu155@gmail.com>
2026-07-17 18:10:17 +0300

    Pip Basics Example

---
+++ 08_modules_packages/139_pip_basics_example.py
@@ -0,0 +1,29 @@
+print("""
+pip is the Python package installer.
+
+Common commands:
+
+Install a package:
+    pip install requests
+
+Install a specific version:
+    pip install requests==2.31.0
+
+Uninstall:
+    pip uninstall requests
+
+List installed packages:
+    pip list
+
+Show package info:
+    pip show requests
+
+Save dependencies:
+    pip freeze > requirements.txt
+
+Install from requirements file:
+    pip install -r requirements.txt
+
+Upgrade a package:
+    pip install --upgrade requests
+""")
